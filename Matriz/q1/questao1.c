8fb5d10a959ce6d4ab7b78e2550551df7954ddae
tatiany <tatianysouza2018@gmail.com>
2023-10-22 16:10:43 -0300

    detalhes

--- Matriz/q1/questao1.c
+++ Matriz/q1/questao1.c
@@ -6,11 +6,9 @@ void main() {
     // matriz 3 por 2
     int vet1[3*2] = {1, 2, 3, 4, 5, 6};
     imprimir(vet1, 3, 2);
-
     // matriz 2 por 3
     int vet2[2*3] = {1, 4, 3, 2, 1, 2};
     imprimir(vet2, 2, 3);
-    
     produto_Matriz(vet1, vet2, 3, 3) ;
     getch();
     }
